f58f9bba2b0874e57a646f1190826276fd956fb1
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-29 00:54:14 +0000

    basic styling done

--- components/Sections/ProjectsSection.js
+++ components/Sections/ProjectsSection.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import IsoTopeGrid from 'react-isotope'
 
+import { useMediaQuery } from 'react-responsive'
+
+
 const Container = styled.div`
     position: relative;
     margin-top: 20px;
+    min-height: 500px;
 `;
 
 const Filter = styled.button`
@@ -24,25 +28,96 @@ const Filter = styled.button`
     }
 
     > label {
-        padding: 5px;
+        margin: 15px 15px 15px 0;
+        padding: 6px 20px;
+        display: inline-block;
+        font-size: 12px;
+        line-height: 20px;
+        border-radius: 50px;
+        background-color: #ffecfb;
+        margin-bottom: 5px;
+        transition: all 0.3s ease-in-out;
         border-bottom: 2px solid transparent;
 
         &:hover {
-            color: blue;
+            background: #F7BD00;
+            color: #fff;
             
         }
     }
+    
 `;
 
 const Card = styled.div`
 
-    &.test {
-        background: blue !important;
+    
+`;
+
+const CardContent = styled.div`
+    overflow: hidden;
+    position: relative;
+    border-radius: 6px;
+    margin: 0;
+
+    img {
+        width: 100%;
+        max-height: 100%;
+        height: auto;
     }
-    &.test1 {
-        background: red !important;
+
+    &:hover img {
+        opacity: 0.4;
+        transition: 0.3s;
+    }
+
+    &:hover {
+        background: red;
+        .project-info {
+        opacity: 1 ;
+    }
+    }
+
+    
+`;
+
+const ProjectInfo = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    opacity: 0;
+    transition: 0.2s linear;
+
+    h4 {
+        font-size: 22px;
+        line-height: 1px;
+        font-weight: 700;
+        margin-bottom: 0;
+    }
+
+    h4 a {
+        color: #fff;
+    }
+
+    h4 a:hover {
+        color: #007bff;
+    }
+    p {
+        padding: 0;
+        margin: 0;
+        color: #fff;
+        font-weight: 500;
+        font-size: 14px;
+        text-transform: uppercase;
     }
 `;
+
 
 const cardsDefault = [
     {
@@ -107,7 +182,9 @@ const ProjectSection = () => {
 
     return (
         <>
-            <div>
+            <div style={{
+                textAlign: 'center'
+            }}>
                 {filters.map(f => (
                     <Filter key={`${f.label}_key`}>
                         <input
@@ -128,22 +205,21 @@ const ProjectSection = () => {
                     gridLayout={cardsDefault}
                     noOfCols={4}
                     unitWidth={200}
-                    unitHeight={100}
+                    unitHeight={350}
                     filters={filters}
-                    style={{
-                        display: 'flex !important'
-                    }}
                 >
                     {cardsDefault.map(card => (
                         <Card key={card.id} className={`card ${card.filter[0]}`}>
-                            <div className="tags">
-                                {card.filter.map((c, index) => (
-                                    <span key={index}>{c}</span>
-                                ))}
-                            </div>
-                            {card.id}
+                            <CardContent>
+                                <img src="/img/team-4.jpg" />
+                                <ProjectInfo className="project-info">
+                                    <h4><a href="#">Projet 1</a></h4>
+                                    <p>Description</p>
+                                </ProjectInfo>
+                            </CardContent>
                         </Card>
                     ))}
+
                 </IsoTopeGrid>
             </Container>
 
