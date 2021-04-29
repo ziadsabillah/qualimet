0a4dd3373e49f5c9bb2450de507ca83224f94e6c
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-29 11:57:20 +0000

    finished contact section

--- components/Sections/Contact.js
+++ components/Sections/Contact.js
@@ -24,6 +24,10 @@ const LeftColumn = styled.div`
     @media (min-width: 992px) {
         flex: 66.666667%;
     }
+
+    @media (max-width: 500px) {
+        margin-top: 1.4em;
+    }
     flex: 100%;
     padding-right: 15px;
     padding-left: 15px;
@@ -35,10 +39,71 @@ const RightColumn = styled.div`
     @media (min-width: 992px) {
         flex: 33.33333%;
     }
+
+    @media (max-width: 500px) {
+        margin-top: 1.4em;
+        padding: 10px;
+    }
     flex: 100%;
     padding-right: 15px;
     padding-left: 15px;
 `;
+
+const InfoTitle = styled.h2`
+    position: relative;
+    color: #242424;
+    font-size: 26px;
+    font-weight: 600;
+    padding-bottom: 12px;
+    margin-bottom: 40px;
+
+    &::after {
+        position: absolute;
+        content: '';
+        left: 0px;
+        bottom: 0px;
+        width: 40px;
+        height: 2px;
+        background-color: #f7bd00;
+    }
+`;
+
+const InfoList = styled.ul`
+    position: relative;
+
+    li {
+        position: relative;
+        margin-bottom: 15px;
+        padding-left: 40px;
+        color: #242424;
+        font-weight: 500;
+        font-size: 16px;
+        font-family: 'Poppins', sans-serif;
+    }
+`;
+
+const SmallHeader = styled.h3`
+    
+    position: relative;
+    color: #242424;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 1.4em;
+    margin-top: 50px;
+`;
+
+const Times = styled.div`
+    position: relative;
+    color: #797979;
+    font-size: 15px;
+    font-weight: 500;
+    padding-left: 40px;
+    margin-top: 8px;
+    line-height: 2em;
+    font-family: 'Poppins', sans-serif;
+`;
+
+
 
 
 const Contact = () => {
@@ -95,7 +160,21 @@ const Contact = () => {
                             </Form>
                         </LeftColumn>
                         <RightColumn>
+                            <InfoTitle>Borvel SARL</InfoTitle>
+                            <InfoList>
+                                <li>Avenue Mohamed V</li>
+                                <li>+212 6 13 04 93 93</li>
+                                <li>exemple@exemple.com</li>
+                            </InfoList>
+                            <SmallHeader>Horaires de travail</SmallHeader>
+                            <Times>
+                                Lun-Merc : 10:00 au 18:00
+                                <br />
+                                Jeu-Vend : 10:00 au 14:00
+                                <br />
+                                Sam-Dim : 10:00 au 12:00
 
+                            </Times>
                         </RightColumn>
                     </Row>
                 </AutoContainer>
