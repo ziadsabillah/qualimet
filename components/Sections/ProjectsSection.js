651fdcb645df42830ec4c4768e051887027ed5d7
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-29 17:14:36 +0000

    removed some unused imports

--- components/Sections/ProjectsSection.js
+++ components/Sections/ProjectsSection.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 import styled from 'styled-components'
-import IsoTopeGrid from 'react-isotope'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 import AutoContainer from '../AutoContainer'
