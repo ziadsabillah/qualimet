408f76ad41798c31b3331eadd604dbfa10ceb249
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-30 15:15:42 +0000

    Added about page skeleton

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+
+export default function About() {
+    return (
+        <>
+            <Layout>
+                
+            </Layout>
+        </>
+    )
+}
