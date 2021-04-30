4003b1b6c27b1288b1ceb09c9c46e1526bf23c8e
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-30 20:02:51 +0000

    added contact page ssr

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import Contact from '../components/Sections/Contact'
+
+export default function ContactPage() {
+    return (
+        <>
+        <Layout>
+            <Contact />
+        </Layout>
+        </>
+    )
+}
