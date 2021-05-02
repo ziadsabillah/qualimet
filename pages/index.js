80e8e7205927b3118b0ecc3f54108e1069f71aaa
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-02 22:16:53 +0000

    removed unnecessary lazy loading for homescreen

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import dynamic from 'next/dynamic'
-
 import Layout from '../components/Layout'
 
-const DynamicProjectSection = dynamic(() => import('../components/Sections/ProjectsSection'), {loading: () => <p>...</p>})
-const DynamicTestimonials = dynamic(() => import('../components/Sections/Testimonials') , {loading: () => <p>...</p>})
-const DynamicVideoSection = dynamic(() => import('../components/Sections/VideoSection'), {loading: () => <p>...</p>})
-const DynamicContact = dynamic(() => import('../components/Sections/Contact'), {loading: () => <p>...</p>})
-const DynamicHero = dynamic(() => import('../components/Hero'), {loading: () => <p>...</p>})
-const DynamicCallToAction = dynamic(() => import('../components/Sections/CallToActionSection'), {loading: () => <p>...</p>})
+import ProjectsSection from '../components/Sections/ProjectsSection'
+import Testimonials from '../components/Sections/Testimonials'
+import VideoSection from '../components/Sections/VideoSection'
+import Contact from '../components/Sections/Contact'
+import Hero from '../components/Hero'
+import CallToActionSection from '../components/Sections/CallToActionSection'
 
 export default function Home() {
   return (
     <Layout>
-      <DynamicHero />
-      <DynamicVideoSection />
-      <DynamicProjectSection />
-      <DynamicCallToAction />
-      <DynamicTestimonials />
-      <DynamicContact />
+      <Hero />
+      <VideoSection />
+      <ProjectsSection />
+      <CallToActionSection />
+      <Testimonials />
+      <Contact />
     </Layout>
   )
 }
