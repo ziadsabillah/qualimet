af8311a2d2d503b838a8ea724c0ec2c9d145c849
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-18 17:04:23 +0100

    added recrutement mail

---
+++ data/contact.js
@@ -0,0 +1,5 @@
+const contact = {
+    email: 'ziad.lunasoft@gmail.com',
+};
+
+export default contact;
--- pages/recrutements.js
+++ pages/recrutements.js
@@ -1,11 +1,32 @@
 import Layout from '../components/Layout'
 
+import { SectionHeader } from '../components/SectionHeader'
+import AutoContainer from '../components/AutoContainer'
+import { SubmitButton } from '../components/General'
+
+import contact from '../data/contact'
+
 export default function RecrutementPage() {
     return (
         <>
-        <Layout title="Recrutements">
-            
-        </Layout>
+            <Layout title="Recrutements">
+                <AutoContainer>
+                    <SectionHeader
+                        style={{ textAlign: 'center', margin: '3em 0' }}>
+                        <h2>Rejoignez-nous</h2>
+                        <p>Rejoignez notre equipe en nous envoyons votre CV!</p>
+                    </SectionHeader>
+                    <div style={{ textAlign: 'center', width: '100%', margin: '2em 0' }}>
+                        <form action={`mailto:${contact.email}`} target="_blank" method="get">
+                            <SubmitButton>
+                                Envoyer votre CV
+                            </SubmitButton>
+                        </form>
+
+                    </div>
+                </AutoContainer>
+
+            </Layout>
         </>
     )
 }
