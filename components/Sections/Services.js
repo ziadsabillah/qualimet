f9baaa31cf6ff4459e3533f0e2aea3d503bc32d4
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-23 16:44:40 +0100

    added services component skeleton

--- components/Sections/Services.js
+++ components/Sections/Services.js
@@ -1,11 +1,41 @@
 import styled from 'styled-components';
+import Carousel from 'react-multi-carousel';
+import { Row, Col } from 'react-bootstrap';
 
 
 
+const Wrapper = styled.section`
+
+    padding: 100px 0;
+
+`;
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        paritialVisibilityGutter: 60
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1,
+        paritialVisibilityGutter: 50
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        paritialVisibilityGutter: 30
+    }
+};
 
 const Services = () => {
+
     return (
         <>
+
+        <Wrapper>
+        </Wrapper>
+
         </>
     )
 }
