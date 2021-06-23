0fb3f34a3a08fa6d09b811aea0bbc18f2c90a3df
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-23 17:31:30 +0100

    improved services section

--- components/Sections/Services.js
+++ components/Sections/Services.js
@@ -6,18 +6,38 @@ const Content = styled.div`
     position: relative;
     display: flex;
     flex-direction: column;
-    padding: 4em;
+    padding: 3em 1em 0 1em;
     align-items: center;
+
 `;
 
 const Header = styled.h1`
     font-size: 1.8em;
     color: #242424;
+    font-weight: bold;
+    font-family: 'Gotham Bold', sans-serif;
 `;
 
 const Paragraph = styled.p`
-    font-size: 1.8em;
+    margin-top: 3em;
+    font-size: 1.4em;
     color: grey;
+`;
+
+const Button = styled.a`
+    margin-top: 2.5em;
+    padding: .5em 1.5em;
+    color: #242424;
+    font-weight: bold;
+    border-radius: 4px;
+    text-decoration: none;
+    border: 1px solid #F68C33;
+    cursor: pointer;
+    transition: background .3s ease-in;
+    &:hover, &:focus {
+        color: #FFF;
+        background-color: #F68C33;
+    }
 `;
 
 
@@ -25,7 +45,7 @@ const Services = () => {
 
     return (
         <>
-            <Carousel nextLabel="Suivant" prevLabel="Precedant">
+            <Carousel nextLabel="" prevLabel="">
                 <Carousel.Item>
                     <Row>
                         <Col sm={12} md={6}>
@@ -39,6 +59,7 @@ const Services = () => {
                                 <Paragraph>
                                 Nous vous offrons des prestations d'étalonnage conformément à la norme ISO 17025 des instruments et matériels utilisés dans les domaines suivant : électricité et magnétisme, force, couple, pression, température, masse, volume, aérométrie, verrerie volumétriques, dimensionnel etc.
                                 </Paragraph>
+                                <Button>Lire Plus</Button>
                             </Content>
                         </Col>
                     </Row>
