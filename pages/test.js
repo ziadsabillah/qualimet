46411c66d82a1093ba4eceb42ff872a9a27f9a8c
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-24 17:27:52 +0100

    landing page finished

---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+
+
+export default function UnderConstructionPage() {
+    return (
+        <>
+            <Layout title="Ce site est sous developpment">
+                <img src="/const.jpg" alt="Under construction" width="100%" height="100%" />
+            </Layout>
+        </>
+    )
+}
